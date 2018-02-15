283e75cc6c2bf9d09bef5ceba019242c186a4f04
Yuji Takaesu <taka.esu.user@gmail.com>
2018-02-15 10:56:54 +0900

    Allow function nest samples

--- src/allow_function.js
+++ src/allow_function.js
@@ -1,5 +1,8 @@
 // ES2015の新機能: 関数の新しい書き方「arrow 関数」 - Qiita
 // https://qiita.com/niisan-tokyo/items/4aec6e01c2c8ffc5fc73
+//
+// ES2015(ES6)新構文：アロー関数(Arrow function)｜もっこりJavaScript｜ANALOGIC（アナロジック）
+// http://analogic.jp/arrow-function/
 
 let normalFunc = function(x) {
   console.log(x)
@@ -38,3 +41,16 @@ console.log(arr)
 console.log(arr1)
 console.log(arr2)
 
+
+// allow function の ネスト
+let arrwFunc5 = (x) => (y) => ({ aa: x + y })
+console.log(arrwFunc5(5)) // return Function
+console.log(arrwFunc5(5)(2))
+// 以下のようなfunctionと同じ
+func5 = function(x) {
+  return function(y) {
+    return { bb: x + y }
+  }
+}
+console.log(func5(2))  // return Function
+console.log(func5(2)(3))
